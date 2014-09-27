3184653c48242a9a122712f70e6cb76e3a7a77e8
Lex Robinson <lexi@lexi.org.uk>
2014-09-27 20:51:11 +0100

    Move the sevensegment code out into its own file

--- bomb.ino
+++ bomb.ino
@@ -8,41 +8,12 @@ enum Mode {
 Mode mode;
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
-byte numbers[] = {
-	255 - B00111111, //0
-	255 - B00000110, //1
-	255 - B01011011, //2
-	255 - B01001111, //3
-	255 - B01100110, //4
-	255 - B01101101, //5
-	255 - B01111101, //6
-	255 - B00000111, //7
-	255 - B01111111, //8
-	255 - B01101111, //9
-};
-
-// shift
-int latchPin = 10,
-	dataPin  = 9,
-	clockPin = 8;
-
 void setup() {
 	// mode = Setup;
 	mode = Play;
 	lcd.begin(21, 4);
 
-	pinMode(latchPin, OUTPUT);
-	pinMode(clockPin, OUTPUT);
-	pinMode(dataPin, OUTPUT);
-
-	digitalWrite(clockPin, LOW);
-
-	digitalWrite(latchPin, LOW);
-	// Clear
-	shiftOut(dataPin, clockPin, MSBFIRST, B00000000);
-	shiftOut(dataPin, clockPin, MSBFIRST, B00000000);
-	shiftOut(dataPin, clockPin, MSBFIRST, B00000000);
-	digitalWrite(latchPin, HIGH);
+	sevenseg_setup();
 
 	lcd.setCursor(0, 1);
 	lcd.print("butts");
@@ -55,27 +26,11 @@ void setup() {
 }
 
 void loop() {
-	// lcd.setCursor(0, 0);
-	// lcd.print("aaa 0                ");
-	// delay(1000);
 	byte data = 1, port = 1;
 	for (int j = 0; j < 8; j++) {
-		data = numbers[j];
-		// lcd.setCursor(4, 0);
-		// lcd.print(data, 2);
-
-		digitalWrite(latchPin, LOW);
-		shiftOut(dataPin, clockPin, MSBFIRST, port);
-		shiftOut(dataPin, clockPin, MSBFIRST, data);
-		digitalWrite(latchPin, HIGH);
-		// delay(1);
-		port <<= 1;
+		sevenseg_write(j, j);
 	}
-
-	digitalWrite(latchPin, LOW);
-	shiftOut(dataPin, clockPin, MSBFIRST, 0);
-	shiftOut(dataPin, clockPin, MSBFIRST, 0);
-	digitalWrite(latchPin, HIGH);
+	sevenseg_clear();
 
 	// (note: line 1 is the second row, since counting begins with 0):
 	lcd.setCursor(0, 3);
@@ -87,9 +42,3 @@ void loop() {
 	// 	play_loop();
 	// }
 }
-
-
-// green/white  latch 9
-// orange/brown data  8
-// yellow/black clock 7
-;
---
+++ sevenseg.ino
@@ -0,0 +1,64 @@
+
+
+byte numbers[] = {
+	255 - B00111111, //0
+	255 - B00000110, //1
+	255 - B01011011, //2
+	255 - B01001111, //3
+	255 - B01100110, //4
+	255 - B01101101, //5
+	255 - B01111101, //6
+	255 - B00000111, //7
+	255 - B01111111, //8
+	255 - B01101111, //9
+};
+byte digits[] = {
+	255 - B11111110,
+	255 - B11111101,
+	255 - B11111011,
+	255 - B11110111,
+	255 - B11101111,
+	255 - B11011111,
+	255 - B10111111,
+	255 - B01111111,
+};
+
+// shift
+int latchPin = 10,
+	dataPin  = 9,
+	clockPin = 8;
+
+// green/white  latch 9
+// orange/brown data  8
+// yellow/black clock 7
+
+void sevenseg_setup() {
+	pinMode(latchPin, OUTPUT);
+	pinMode(clockPin, OUTPUT);
+	pinMode(dataPin, OUTPUT);
+
+	digitalWrite(clockPin, LOW);
+
+	digitalWrite(latchPin, LOW);
+	// Clear
+	shiftOut(dataPin, clockPin, MSBFIRST, B00000000);
+	shiftOut(dataPin, clockPin, MSBFIRST, B00000000);
+	shiftOut(dataPin, clockPin, MSBFIRST, B00000000);
+	digitalWrite(latchPin, HIGH);
+}
+
+void sevenseg_write(int which, int what) {
+	byte data = numbers[what];
+	byte port = digits[which];
+	digitalWrite(latchPin, LOW);
+	shiftOut(dataPin, clockPin, MSBFIRST, port);
+	shiftOut(dataPin, clockPin, MSBFIRST, data);
+	digitalWrite(latchPin, HIGH);
+}
+
+void sevenseg_clear() {
+	digitalWrite(latchPin, LOW);
+	shiftOut(dataPin, clockPin, MSBFIRST, 0);
+	shiftOut(dataPin, clockPin, MSBFIRST, 0);
+	digitalWrite(latchPin, HIGH);
+}
