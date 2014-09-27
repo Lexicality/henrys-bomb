6422f71543338a444c18e68ca40e4bfe45b4d291
Lex Robinson <lexi@lexi.org.uk>
2014-09-27 19:43:26 +0100

    Display 0-7 constantly

--- bomb.ino
+++ bomb.ino
@@ -55,32 +55,37 @@ void setup() {
 }
 
 void loop() {
-	lcd.setCursor(0, 0);
-	lcd.print("aaa 0                ");
-	delay(1000);
-	byte data = 1;
-	for (int j = 0; j < 10; j++) {
+	// lcd.setCursor(0, 0);
+	// lcd.print("aaa 0                ");
+	// delay(1000);
+	byte data = 1, port = 1;
+	for (int j = 0; j < 8; j++) {
 		data = numbers[j];
-		lcd.setCursor(4, 0);
-		lcd.print(data, 2);
+		// lcd.setCursor(4, 0);
+		// lcd.print(data, 2);
 
 		digitalWrite(latchPin, LOW);
-		shiftOut(dataPin, clockPin, MSBFIRST, B00000010);
+		shiftOut(dataPin, clockPin, MSBFIRST, port);
 		shiftOut(dataPin, clockPin, MSBFIRST, data);
 		digitalWrite(latchPin, HIGH);
-		delay(1000);
-		// data <<= 1;
+		// delay(1);
+		port <<= 1;
 	}
+
+	digitalWrite(latchPin, LOW);
+	shiftOut(dataPin, clockPin, MSBFIRST, 0);
+	shiftOut(dataPin, clockPin, MSBFIRST, 0);
+	digitalWrite(latchPin, HIGH);
 
 	// (note: line 1 is the second row, since counting begins with 0):
 	lcd.setCursor(0, 3);
 	// print the number of seconds since reset:
 	lcd.print(millis()/1000);
-	if (mode == Setup) {
-		setup_loop();
-	} else {
-		play_loop();
-	}
+	// if (mode == Setup) {
+	// 	setup_loop();
+	// } else {
+	// 	play_loop();
+	// }
 }
 
 
