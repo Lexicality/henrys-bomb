c019b01d48d1e5956c569dd5dfc2e240e80748cb
Lex Robinson <lexi@lexi.org.uk>
2014-09-27 18:49:27 +0100

    Attempt to SPI

--- bomb.ino
+++ bomb.ino
@@ -1,4 +1,5 @@
 #include <LiquidCrystal.h>
+#include <SPI.h>
 
 enum Mode {
 	Setup,
@@ -9,26 +10,27 @@ Mode mode;
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
 // shift
-int latchPin = 12,//9,
-	dataPin  = 11,//8,
-	clockPin = 10;//7;
+int latchPin = 9;
 
 void setup() {
 	// mode = Setup;
 	mode = Play;
 	lcd.begin(21, 4);
 
+	// Latch for 7seg
 	pinMode(latchPin, OUTPUT);
-	pinMode(clockPin, OUTPUT);
-	pinMode(dataPin, OUTPUT);
 
-	digitalWrite(clockPin, LOW);
+	// initialize SPI:
+	SPI.begin();
+	SPI.setBitOrder(MSBFIRST);
 
 	digitalWrite(latchPin, LOW);
-	// Clear
-	shiftOut(dataPin, clockPin, MSBFIRST, B00000000);
-	shiftOut(dataPin, clockPin, MSBFIRST, B00000000);
-	shiftOut(dataPin, clockPin, MSBFIRST, B00000000);
+	SPI.transfer(B00000000);
+	SPI.transfer(B00000000);
+	SPI.transfer(B00000000);
+
+	SPI.transfer(B11111000);
+	SPI.transfer(B000000010);
 	digitalWrite(latchPin, HIGH);
 
 	lcd.setCursor(0, 1);
@@ -44,33 +46,34 @@ void setup() {
 void loop() {
 	lcd.setCursor(0, 0);
 	lcd.print("aaa 0                ");
-	digitalWrite(latchPin, LOW);
-	shiftOut(dataPin, clockPin, MSBFIRST, B00000001);
-	shiftOut(dataPin, clockPin, MSBFIRST, B00000000);
-	digitalWrite(latchPin, HIGH);
-	delay(1000);
-	byte data = 1;
-	for (int j = 0; j <= 8; j++) {
-		lcd.setCursor(4, 0);
-		lcd.print(data, 2);
+	// digitalWrite(latchPin, LOW);
+	// shiftOut(dataPin, clockPin, MSBFIRST, B00000001);
+	// shiftOut(dataPin, clockPin, MSBFIRST, B00000000);
+	// digitalWrite(latchPin, HIGH);
+	// delay(1000);
+	// byte data = 1;
+	// for (int j = 0; j <= 8; j++) {
+	// 	lcd.setCursor(4, 0);
+	// 	lcd.print(data, 2);
 
-		digitalWrite(latchPin, LOW);
-		shiftOut(dataPin, clockPin, MSBFIRST, B00000001);
-		shiftOut(dataPin, clockPin, MSBFIRST, data);
-		digitalWrite(latchPin, HIGH);
-		delay(1000);
-		data <<= 1;
-	}
+	// 	digitalWrite(latchPin, LOW);
+	// 	shiftOut(dataPin, clockPin, MSBFIRST, B00000001);
+	// 	shiftOut(dataPin, clockPin, MSBFIRST, data);
+	// 	digitalWrite(latchPin, HIGH);
+	// 	delay(1000);
+	// 	data <<= 1;
+	// }
 
 	// (note: line 1 is the second row, since counting begins with 0):
 	lcd.setCursor(0, 3);
 	// print the number of seconds since reset:
 	lcd.print(millis()/1000);
-	if (mode == Setup) {
-		setup_loop();
-	} else {
-		play_loop();
-	}
+
+	// if (mode == Setup) {
+	// 	setup_loop();
+	// } else {
+	// 	play_loop();
+	// }
 }
 
 
