a0b8bec26c2bee37718b765b1388c8e96380c814
YetiFiasco <hfsands@googlemail.com>
2014-09-28 12:07:02 +0100

    random number gen and variables
    Tell me how to make multiple variables at once, this is really messy

--- play.ino
+++ play.ino
@@ -1,8 +1,31 @@
+
+
 void play_setup() {
+	randomSeed(analogRead(0)); //Get floating data for random number generator from unused analog pin
+
+
 	lcd.print("world, hello! test");
 
 }
 
 void play_loop() {
-
+	Long AAutoCode1 = random(10000000, 99999999); //Will this change the variable each time the loop is run? Pretty sure it will
+	Long AAutoCode2 = random(10000000, 99999999);
+	Long AAutoCode3 = random(10000000, 99999999);
+	Long AAutoCode4 = random(10000000, 99999999);
+	Long AAutoCode5 = random(10000000, 99999999);
+	Long AAutoCode6 = random(10000000, 99999999);
+	Long AAutoCode7 = random(10000000, 99999999);
+	Long AAutoCode8 = random(10000000, 99999999);
+	Long AAutoCode9 = random(10000000, 99999999);
+	Long DAutoCode1 = random(10000000, 99999999);
+	Long DAutoCode2 = random(10000000, 99999999);
+	Long DAutoCode3 = random(10000000, 99999999);
+	Long DAutoCode4 = random(10000000, 99999999);
+	Long DAutoCode5 = random(10000000, 99999999);
+	Long DAutoCode6 = random(10000000, 99999999);
+	Long DAutoCode7 = random(10000000, 99999999);
+	Long DAutoCode8 = random(10000000, 99999999);
+	Long DAutoCode9 = random(10000000, 99999999);
+	
 }
--- program.ino
+++ program.ino
@@ -1,9 +1,17 @@
 
 
 void program_setup() {
-	lcd.print("hello, world!");
+	lcd.print("Program Mode");
 }
 
 void program_loop() {
+	// Set game mode (A,B,C,D)
+	// Set key cards (on/off)
+	// Set code method (Automatically generated/manual)
+	// Set no. of activation codes (numeric)
+	// Set no. of deactivation codes (numeric)
+	// Set strobe (on/off)
+	// Set piezos (on/off)
+	// Commit all variables to flash
 
 }
