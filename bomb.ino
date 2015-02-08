a4dd4363b35e4e3fbb71f5d324e3dc3b308e8058
Lex Robinson <lexi@lexi.org.uk>
2015-02-08 17:00:22 +0000

    Use characters so the setup system can understand them

--- bomb.ino
+++ bomb.ino
@@ -13,11 +13,18 @@ LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //four columns
 //define the cymbols on the buttons of the keypads
+//char hexaKeys[ROWS][COLS] = {
+//	{ 1, 2, 3, 10 },
+//	{ 4, 5, 6, 11 },
+//	{ 7, 8, 9, 12 },
+//	{ 0, -1, 0, 13 }, // * 0 #
+//};
+char nul = 0;
 char hexaKeys[ROWS][COLS] = {
-	{ 1, 2, 3, 10 },
-	{ 4, 5, 6, 11 },
-	{ 7, 8, 9, 12 },
-	{ 0, -1, 0, 13 }, // * 0 #
+	{ '1', '2', '3', 'a' },
+	{ '4', '5', '6', 'b' },
+	{ '7', '8', '9', 'c' },
+	{ '*', '0', '#', 'd' }, // * 0 #
 };
 //byte rowPins[ROWS] = { 22, 23, 24, 25 }; //connect to the row pinouts of the keypad
 //byte colPins[COLS] = { 26, 27, 28, 29 }; //connect to the column pinouts of the keypad
@@ -66,16 +73,16 @@ char lastKey = 0;
 
 void loop()
 {
-	char customKey = customKeypad.getKey();
-	if (customKey) {
-		if (customKey < 0)
-			customKey = 0;
-		lastKey = customKey;
-	}
+	//char customKey = customKeypad.getKey();
+	//if (customKey) {
+	//	if (customKey < 0)
+	//		customKey = 0;
+	//	lastKey = customKey;
+	//}
 	// lcd.setCursor(0, 0);
 	// lcd.print(lastKey);
 
-	sevenseg_write(0, lastKey);
+	//sevenseg_write(0, lastKey);
 	// sevenseg_clear();
 
 	// (note: line 1 is the second row, since counting begins with 0):
